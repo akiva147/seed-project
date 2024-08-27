42b85d6ca2aa14455807c9f4d11878fc93b81fe2
akiva levi <akiva147@gmail.com>
2024-08-27 12:50:37 +0300

    feat(turborepo, switchboad, package.json): added env tto turbo.json, remove unused libraries, refactored the Switchboard.tsx

--- apps/web/src/routes/Switchboard.tsx
+++ apps/web/src/routes/Switchboard.tsx
@@ -1,15 +1,50 @@
+import {
+    createBrowserRouter,
+    Navigate,
+    redirect,
+    RouterProvider,
+} from 'react-router-dom';
 import { AuthenticatedRoutes } from './AuthenticatedRoutes';
 import { PublicRoutes } from './PublicRoutes';
+import { Layout } from 'src/components/Layout';
+import { BaseErrorBoundary } from 'src/components/BaseErrorBoundary';
+import { Notes } from 'src/components/Notes';
+import { AccountInfo, UserContext } from 'src/contexts/UserProvider';
+import { useContext } from 'react';
+import { LoginPage } from 'src/pages/LoginPage';
 
 export interface SwitchboardProps {}
 
-export const Switchboard: React.FC<SwitchboardProps> = () => (
-    <>
-        {/* <AuthenticatedTemplate> */}
-        <AuthenticatedRoutes />
-        {/* </AuthenticatedTemplate> */}
-        {/* <UnauthenticatedTemplate> */}
-        <PublicRoutes />
-        {/* </UnauthenticatedTemplate> */}
-    </>
-);
+const getRouter = (currentUser: AccountInfo | undefined) =>
+    createBrowserRouter([
+        {
+            path: '/',
+            element: <Layout />,
+            errorElement: <BaseErrorBoundary />,
+            loader: async () => {
+                if (!currentUser) {
+                    throw redirect('/login');
+                }
+
+                return { currentUser };
+            },
+            children: [
+                { path: '/', element: <Navigate to="/notes" /> },
+                { path: '/notes', element: <Notes /> },
+            ],
+        },
+        {
+            path: '/login',
+            element: <LoginPage />,
+        },
+        {
+            path: '*',
+            element: <Navigate to="/login" />,
+        },
+    ]);
+
+export const Switchboard: React.FC<SwitchboardProps> = () => {
+    const { currentUser } = useContext(UserContext);
+
+    return <RouterProvider router={getRouter(currentUser)} />;
+};
